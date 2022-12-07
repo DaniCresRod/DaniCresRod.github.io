3abd0d7663205665944613d709c2fd0af7a56777
Dani C <dani.crespo.rodriguez@gmail.com>
2022-12-07 15:19:58 +0100

    CSS y JS comentados

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Funcion que muestra/esconde un div con informacion al hacer click en el nombre de cada persona
+//Lo hago alternando la clase CSS "invisible" entre las asignadas a la capa que quiero mostrar/ocultar
+//Esta capa tiene el atributo "display:none"
+
 let nomKevin = document.getElementById("nombreKevin");
 let nomDani = document.getElementById("nombreDani");
 
@@ -5,6 +9,8 @@ let fichKevin=document.getElementById("fichaKevin");
 let fichDani=document.getElementById("fichaDani");
 
 /*
+Primero la hice con una funcion anonima para cada click
+
 nomKevin.addEventListener("click", ()=>{
     if(fichKevin.classList.contains("invisible")){
         fichKevin.classList.remove("invisible");
@@ -29,6 +35,7 @@ nomDani.addEventListener("click", ()=>{
 });
 */
 
+//Aqui lo mismo con un diseño algo mas depurado
 nomKevin.addEventListener("click", function(){cambiaInfo(fichKevin, fichDani)});
 nomDani.addEventListener("click", function(){cambiaInfo (fichDani, fichKevin)});
 
@@ -42,7 +49,9 @@ function cambiaInfo( fichaActiva, fichaInactiva){
     }
 }
 
-//Esconde todas las fichas del navegador
+//Esconde todas las fichas del navegador y muestra solo la que se clica
+//Funcion parecida a la anterior para segir con la practica
+//Esta vez uso el atributo z-index para mostrar/ocultar las capas
 let elijoHTML=document.querySelectorAll("#menusNavegacion p");
 elijoHTML[0].addEventListener("click", function(){escondefichas(0)});
 elijoHTML[1].addEventListener("click", function(){escondefichas(1)});
@@ -61,6 +70,8 @@ function escondefichas(numero){
    }
 }
 
+//Funcion que muestra una ventana emergente cuando la pantalla es menor de 100px
+//para completar el responsive
 window.addEventListener("resize", function(){
     if(window.innerHeight<100){
         alert("bastante pequeña ya, no??");        
